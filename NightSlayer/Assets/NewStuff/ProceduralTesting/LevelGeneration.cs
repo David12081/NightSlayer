b1b9784d9aa41ba253eede1d282de017af39bccb
David12081 <84799955+David12081@users.noreply.github.com>
2023-04-11 19:27:17 -0600

    Level Generation Commit

--- NightSlayer/Assets/NewStuff/ProceduralTesting/LevelGeneration.cs
+++ NightSlayer/Assets/NewStuff/ProceduralTesting/LevelGeneration.cs
@@ -16,7 +16,11 @@ public class LevelGeneration : MonoBehaviour
     public float minX;
     public float maxX;
     public float minY;
-    private bool stopGeneration;
+    public bool stopGeneration;
+
+    public LayerMask room;
+
+    private int downCounter;
 
     private void Start()
     {
@@ -38,6 +42,11 @@ public class LevelGeneration : MonoBehaviour
         {
             timeBtwRoom -= Time.deltaTime;
         }
+
+        if(stopGeneration == true)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void Move()
@@ -46,6 +55,8 @@ public class LevelGeneration : MonoBehaviour
         {
             if(transform.position.x < maxX)
             {
+                downCounter = 0;
+
                 Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
                 transform.position = newPos;
 
@@ -71,6 +82,8 @@ public class LevelGeneration : MonoBehaviour
         {
             if (transform.position.x > minX)
             {
+                downCounter = 0;
+
                 Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
                 transform.position = newPos;
 
@@ -86,8 +99,31 @@ public class LevelGeneration : MonoBehaviour
         }
         else if(direction == 5)
         {
+            downCounter++;
+            
             if(transform.position.y > minY)
             {
+                Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, room);
+                if(roomDetection.GetComponent<RoomType>().type != 1 && roomDetection.GetComponent<RoomType>().type != 3)
+                {
+                    if(downCounter >= 2)
+                    {
+                        roomDetection.GetComponent<RoomType>().RoomDestruction();
+                        Instantiate(rooms[3], transform.position, Quaternion.identity);
+                    }
+                    else
+                    {
+                        roomDetection.GetComponent<RoomType>().RoomDestruction();
+
+                        int randBottomRoom = Random.Range(1, 4);
+                        if (randBottomRoom == 2)
+                        {
+                            randBottomRoom = 1;
+                        }
+                        Instantiate(rooms[randBottomRoom], transform.position, Quaternion.identity);
+                    }
+                }
+                
                 Vector2 newPos = new Vector2(transform.position.x, transform.position.y - moveAmount);
                 transform.position = newPos;
 
@@ -100,8 +136,6 @@ public class LevelGeneration : MonoBehaviour
             {
                 stopGeneration = true;
             }
-        }
-
-        Instantiate(rooms[0], transform.position, Quaternion.identity);
+        } 
     }
 }
---
+++ NightSlayer/Assets/NewStuff/ProceduralTesting/SpawnRoom.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRoom : MonoBehaviour
+{
+    public LayerMask room;
+    public LevelGeneration levelGeneration;
+    [SerializeField] GameObject roomClosed;
+    
+    void Update()
+    {
+        Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, room);
+        if(roomDetection == null && levelGeneration.stopGeneration == true)
+        {
+            Instantiate(roomClosed, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        else if(roomDetection != null && levelGeneration.stopGeneration == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
